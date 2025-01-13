1e116922b820d92acadab76cdc6cc6e2bd44d88a
CongTraanApollogix <frontend_dev@apollogix.net>
2025-01-13 17:32:31 +0700

    add

---
+++ coffee_fee/lib/Image/Icon.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { SvgIconTypeMap } from '@mui/material';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
+import { CSSProperties, memo } from 'react';
+
+interface IProps {
+  icon: OverridableComponent<SvgIconTypeMap<object, 'svg'>>; // Không cần `muiName`
+  className?: string;
+  style?: CSSProperties;
+}
+
+const KICon: React.FC<IProps> = ({ icon: IconComponent, style, ...rest }) => {
+  if (!IconComponent) {
+    console.error('Provided icon is not a valid React component.');
+    return null;
+  }
+
+  return <IconComponent {...rest} style={style} />;
+};
+
+export default memo(KICon);
---
+++ coffee_fee/lib/Input/index.tsx
@@ -0,0 +1,7 @@
+import BaseInput from './Base';
+
+class KInput {
+  static Base = BaseInput;
+}
+
+export default KInput;
