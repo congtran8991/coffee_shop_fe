7aca9ddcd07fc8c2116e1f64d59b93ffbc41f954
CongTraanApollogix <frontend_dev@apollogix.net>
2025-02-26 17:28:37 +0700

    add code

--- coffee_fee/app/example/page.tsx
+++ coffee_fee/app/example/page.tsx
@@ -8,28 +8,35 @@ import { showToast } from '@/utils/common';
 import PortalHandle from '@/utils/portal';
 
 import * as yup from 'yup';
-
 import Image from 'next/image';
 import { useTranslation } from 'react-i18next';
 import { useResolverForm } from '@/hooks/lib/useResolverForm';
 import { Controller } from 'react-hook-form';
+import Grid from '@mui/material/Grid2';
+import { Button } from '@mui/material';
+
 export default function Home() {
   const count = useAppSelector((state) => state.abc.count);
   const { t, i18n } = useTranslation();
   const dispatch = useAppDispatch();
 
-  const methods = useResolverForm<{ username: string }>({
+  const methods = useResolverForm<{ username: string; password: string }>({
     schema: yup.object().shape({}),
     configs: {
       values: {
         username: '',
+        password: '',
       },
     },
   });
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <button
+    <div
+      className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center mt-40"
+
+      // className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-10 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
+    >
+      {/* <button
         onClick={() => {
           PortalHandle.popup.open({
             title: 'popup',
@@ -46,28 +53,43 @@ export default function Home() {
         }}
       >
         Call Popup
-      </button>
-      <KInput.Base
-        name="test"
-        label="Text"
-        message="shhsvh"
-        // sx={{ color: KColors.primary.moderate }}
-      />
+      </button> */}
+
       <div>{localStorage.getItem('i18nextLng')}</div>
 
-      <KForm onSubmit={() => {}}>
-        <Controller
-          name="username"
-          control={methods.control}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <KInput.Base
-              name="test"
-              label="Text"
-              message="shhsvh"
-              // sx={{ color: KColors.primary.moderate }}
+      <KForm
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log('submit');
+        }}
+      >
+        <Grid maxWidth={'400px'} container spacing={2}>
+          <Grid size={{ xs: 12 }}>
+            <Controller
+              name="username"
+              control={methods.control}
+              render={({ field }) => (
+                <KInput.Base {...field} fullWidth label={t('username')} />
+              )}
             />
-          )}
-        />
+          </Grid>
+
+          <Grid size={{ xs: 12 }}>
+            <Controller
+              name="password"
+              control={methods.control}
+              render={({ field }) => (
+                <KInput.Base {...field} fullWidth label={t('password')} />
+              )}
+            />
+          </Grid>
+
+          <Grid size={{ xs: 12 }} textAlign={'center'}>
+            <Button variant="contained" type="submit">
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
       </KForm>
     </div>
   );
